b09f7b9b7096816aa056229c1131205e150bed62
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-03 23:28:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,11 +10,11 @@ def batch_badge_creator(arr)
   x
 end
 
-def assign_rooms(arr)
-  x = []
+def assign_rooms(arr
   cnt = 1
-  arr.each do |y|
-    x.push("Hello, #{y}! You'll be assigned to room #{cnt}!")
+  #x = arr.each do |y|
+  #("Hello, #{y}! You'll be assigned to room #{cnt}!")
+  x = arr.each{|y| ("Hello, #{y}! You'll be assigned to room #{cnt}!") cnt += 1}
     cnt += 1
   end
   x
